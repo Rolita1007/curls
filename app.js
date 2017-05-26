7b94bcdc223c4d229a90c20a181f7b4c1ec1d051
rolita1007 <rolita1007@gmail.com>
2017-05-26 12:00:11 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var hbs = require('hbs');
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -15,6 +17,16 @@ var app = express();
 
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
+
+var db = mongoose.connection;
+
+db.on('error', function(err){
+  console.log(err);
+});
+
+db.once('open', function() {
+  console.log("database has been connected!");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -48,6 +60,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(4000, function(){
+  console.log("app listening on port 4000");
 });
 
 module.exports = app;
